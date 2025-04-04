dd8fafcc590828e1d7f8cf2c16f205d8211104f9
Michael OBrien <obrien.michael.t@gmail.com>
2025-04-04 19:23:20 -0400

    Groups related tests in describe blocks

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -15,57 +15,71 @@ describe('WordleBoard', () => {
     await guessInput.trigger('keydown.enter')
   }
 
-  test('A victory message appears when the user make a guess that matches the word of the day', async () => {
-    await playerSubmitsGuess(wordOfTheDay)
-
-    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+  describe("End of the game messages", () => {
+    test('A victory message appears when the user make a guess that matches the word of the day', async () => {
+      await playerSubmitsGuess(wordOfTheDay)
+  
+      expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+    })
+  
+    test('a defeat message appears if the user makes a guess that is incorrect', async () => {
+      await playerSubmitsGuess("WRONG")
+  
+      expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+    })
+  
+    test('No end-of-game message appears if the user has not yet made a guess', async () => {
+  
+      expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+      expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+    })
   })
 
-  test('a defeat message appears if the user makes a guess that is incorrect', async () => {
-    await playerSubmitsGuess("WRONG")
-
-    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+  describe("Word of the day validations", () => {
+    test("If a word of the day does not have exactly 5 letters, a warning is called", async () => {
+      // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
+      console.warn = vi.fn()
+  
+      mount(WordleBoard, {props: {wordOfTheDay: "RABBIT"}})
+  
+      expect(console.warn).toHaveBeenCalled()
+    })
+  
+    test("If a word of the day is not in all uppercase a warning is called", async () => {
+      // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
+      console.warn = vi.fn()
+  
+      mount(WordleBoard, {props: {wordOfTheDay: "lower"}})
+  
+      expect(console.warn).toHaveBeenCalled()
+    })
+  
+    test("If a word of the day is not a real word a warning is called", async () => {
+      // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
+      console.warn = vi.fn()
+  
+      mount(WordleBoard, {props: {wordOfTheDay: "QWERT"}})
+  
+      expect(console.warn).toHaveBeenCalled()
+    })
+  
+    test("No warning is called if the word of the day is a valid 5 letter uppercase word", async () => {
+      // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
+      console.warn = vi.fn()
+  
+      mount(WordleBoard, {props: {wordOfTheDay: "CRACK"}})
+  
+      expect(console.warn).not.toHaveBeenCalled()
+    })
   })
 
-  test('No end-of-game message appears if the user has not yet made a guess', async () => {
+  describe("Player input validations", () => {
+    test("Player input is exactly 5 letters", () => {
 
-    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
-    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
-  })
+    })
 
-  test("If a word of the day does not have exactly 5 letters, a warning is called", async () => {
-    // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
-    console.warn = vi.fn()
-
-    mount(WordleBoard, {props: {wordOfTheDay: "RABBIT"}})
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test("If a word of the day is not in all uppercase a warning is called", async () => {
-    // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
-    console.warn = vi.fn()
-
-    mount(WordleBoard, {props: {wordOfTheDay: "lower"}})
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test("If a word of the day is not a real word a warning is called", async () => {
-    // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
-    console.warn = vi.fn()
-
-    mount(WordleBoard, {props: {wordOfTheDay: "QWERT"}})
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test("No warning is called if the word of the day is a valid 5 letter uppercase word", async () => {
-    // MOCKS THE CONSOLE WARN CALLED MESSAGE (CAN ALSO BE DONE WITH CONSOLE SPY)
-    console.warn = vi.fn()
-
-    mount(WordleBoard, {props: {wordOfTheDay: "CRACK"}})
-
-    expect(console.warn).not.toHaveBeenCalled()
+    test.todo("Player guesses can only be submitted if they are real words")
+    test.todo("Players guesses are not case sensitive")
+    test.todo("Player guesses can only contian letters")
   })
 })
